9cda029469bcda18ef9c4c5be8318e789434136e
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-04 21:55:59 -0600

    Spring-Boot - Crud Oracle - Bootstrap

---
+++ src/main/java/com/mss/springboot/web/app/SpringBootCrudOracleBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOracleBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOracleBootstrapApplication.class, args);
+	}
+
+}
