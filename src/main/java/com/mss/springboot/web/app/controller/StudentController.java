f7fa387ac165a82f340db1432a6ddf4711ed0779
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-05 23:41:09 -0600

    Estudiante Controlador

--- src/main/java/com/mss/springboot/web/app/controller/StudentController.java
+++ src/main/java/com/mss/springboot/web/app/controller/StudentController.java
@@ -1,16 +1,25 @@
 package com.mss.springboot.web.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mss.springboot.web.app.repo.StudentRepo;
 
 
 @Controller
 public class StudentController {
 	
+	@Autowired
+	private StudentRepo repo;
+	
 	@GetMapping("/")
-	public String homePage() {
+	public String homePage(Model model) {
+		model.addAttribute("students", repo.findAll());
 		return "home";
 	}
+	
 	
 	
 	
